f8904ccd3499e8d8a30da81af33c09547740b1d8
Yashah301 <yashah301@gmail.com>
2021-10-05 23:04:50 -0400

    Add project files.

---
+++ QA_Assignment2/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA_Assignment2
+{
+    public class Rectangle
+    {
+        private int width;
+        private int height;
+
+        public Rectangle()
+        {
+            this.width = 1;
+            this.height = 1;
+        }
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Width { get => width; set => width = value; }
+        public int Height { get => height; set => height = value; }
+
+        public int getPerimeter()
+        {
+            var perimeter = 2 * (Width + Height);
+            return perimeter;
+        }
+        public int getArea()
+        {
+            var area = Width * Height;
+            return area;
+        }
+    }
+
+}
