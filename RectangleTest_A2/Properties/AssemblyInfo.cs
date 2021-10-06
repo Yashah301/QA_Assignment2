db7df1b646fd0e845386d27aa0588cd067f62377
Yashah301 <yashah301@gmail.com>
2021-10-05 23:14:38 -0400

    First Test method

---
+++ RectangleTest_A2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RectangleTest_A2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RectangleTest_A2")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67980b54-5317-4eb5-9577-315719a9bb77")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RectangleTest_A2/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using QA_Assignment2;
+
+namespace RectangleTest_A2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDefaultConstructor()
+        {
+            Rectangle rTest = new Rectangle();
+
+            int h = rTest.Height;
+            int w = rTest.Width;
+
+
+            Assert.AreEqual(1, h);
+            Assert.AreEqual(1, w);
+        }
+
+    }
+}
