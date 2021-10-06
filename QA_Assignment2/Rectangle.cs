17d80f2871fb61b2c527d1ea02059208b8e96e89
Yashah301 <yashah301@gmail.com>
2021-10-06 01:19:23 -0400

    Unit Test Methods

--- QA_Assignment2/Rectangle.cs
+++ QA_Assignment2/Rectangle.cs
@@ -9,30 +9,30 @@ namespace QA_Assignment2
     public class Rectangle
     {
         private int width;
-        private int height;
+        private int length;
 
         public Rectangle()
         {
             this.width = 1;
-            this.height = 1;
+            this.length = 1;
         }
         public Rectangle(int width, int height)
         {
-            this.width = width;
-            this.height = height;
+            this.width = width>0?width:1;
+            this.length = height>0?height:1;
         }
-
+        // Getter and Setter
         public int Width { get => width; set => width = value; }
-        public int Height { get => height; set => height = value; }
+        public int Length { get => length; set => length = value; }
 
         public int getPerimeter()
         {
-            var perimeter = 2 * (Width + Height);
+            var perimeter = 2 * (Width + Length);
             return perimeter;
         }
         public int getArea()
         {
-            var area = Width * Height;
+            var area = Width * Length;
             return area;
         }
     }
---
+++ RectangleTest_A2/RectangleUnitTest.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using QA_Assignment2;
+
+namespace RectangleTest_A2
+{
+    [TestClass]
+    public class RectangleUnitTest
+    {
+        [TestMethod]
+        public void TestDefaultConstructor_defaultValue()
+        {
+            Rectangle rTest = new Rectangle();
+
+            int h = rTest.Length;
+            int w = rTest.Width;
+
+
+            Assert.AreEqual(1, h);
+            Assert.AreEqual(1, w);
+        }
+
+        [TestMethod]
+        public void TestparameterizedConstructor_setGivenValue()
+        {
+            Rectangle rTest = new Rectangle(5,10);
+
+            int h = rTest.Length;
+            int w = rTest.Width;
+
+
+            Assert.AreEqual(10, h);
+            Assert.AreEqual(5, w);
+        }
+        [TestMethod]
+        public void TestRectangleArea_withDefaultConstructor()
+        {
+            Rectangle rTest = new Rectangle();
+
+            var rArea = rTest.getArea();
+
+            Assert.AreEqual(1, rArea);
+        }
+
+        [TestMethod]
+        public void TestRectangleArea_withParameterizedConstructor()
+        {
+            Rectangle rTest = new Rectangle(5, 5);
+
+            var rArea = rTest.getArea();
+
+            Assert.AreEqual(25, rArea);
+        }
+
+        [TestMethod]
+        public void TestRectanglePerimeter_withDefaultConstructor()
+        {
+            Rectangle rTest = new Rectangle();
+
+            var rPerimeter = rTest.getPerimeter();
+
+            Assert.AreEqual(4, rPerimeter);
+        }
+
+        [TestMethod]
+        public void TestRectanglePerimeter_withParameterizedConstructor()
+        {
+            Rectangle rTest = new Rectangle(5, 5);
+
+            var rPerimeter = rTest.getPerimeter();
+
+            Assert.AreEqual(20, rPerimeter);
+        }
+
+        [TestMethod]
+        public void TestRectangle_getLength()
+        {
+            Rectangle rTest = new Rectangle(100,20);
+
+            int l = rTest.Length;
+
+            Assert.AreEqual(20, l);
+        }
+
+        [TestMethod]
+        public void TestRectangle_getWidth()
+        {
+            Rectangle rTest = new Rectangle(100, 20);
+
+            int w = rTest.Width;
+
+            Assert.AreEqual(100, w);
+        }
+
+        [TestMethod]
+        public void TestparameterizedConstructor_setNegetiveWidth()
+        {
+            Rectangle rTest = new Rectangle(-5, 5);
+
+            int h = rTest.Length;
+            int w = rTest.Width;
+
+            Assert.AreEqual(5, h);
+            Assert.AreEqual(1, w);
+        }
+
+        [TestMethod]
+        public void TestparameterizedConstructor_setNegetiveHeight()
+        {
+            Rectangle rTest = new Rectangle(10, -5);
+
+            int h = rTest.Length;
+            int w = rTest.Width;
+
+            Assert.AreEqual(1, h);
+            Assert.AreEqual(10, w);
+        }
+    }
+}
